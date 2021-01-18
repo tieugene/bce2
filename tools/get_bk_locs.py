18e5d4f1725cf2703b6c9698f34a567a7927a0fd
Eugene <ti.eugene@gmail.com>
2021-01-18 11:54:34 +0300

    get bk locks stage #1 (get hashes)

--- tools/get_bk_locs.py
+++ tools/get_bk_locs.py
@@ -1,53 +1,73 @@
 #!/opt/local/bin/python3
 """
 Get block info from LevelDB.
-Input: LevelDB path
-Output: bk file+offset (8 bytes per bk)
+Input: block hashes (stdin), LevelDB path
+Output: bk file+offset (2x4=8 bytes per bk)
 
-Test: 15"/630k,
+Test: 80..90"/630k,
 
-Err: 645179 duped
+Args: -i /Users/eugene/tmp/btc/test.hex -l /Users/eugene/tmp/blockchain/blocks/index -o /Users/eugene/tmp/btc/test.bin
+
+TODO:
+- check infile type (txt/gx/*)
 """
 
-import os, sys
 import argparse
-from struct import pack
+import os
+import sys
 from collections import namedtuple
+from struct import pack
+
+# import leveldb
 import plyvel
 
 # consts
 BLOCK_HAVE_DATA = 8
 BLOCK_HAVE_UNDO = 16
 TOOBIGOFFSET = 256 << 20  # 256MB > any blk*.dat
-MAXFILENO = 2999
-header = "Height\tStat\tnTx  \tFile\tDoffset"
-strike = "======\t====\t=====\t====\t========"
 
 # vars
 LdbRec = namedtuple('LdbRec', ['v', 'h', 's', 't', 'f', 'd', 'u'])
+db = None
+TODUMP = False
+MAXBKNO = 2999
 
 
 def eprint(s: str):
+    """
+    Print message to stderr
+    :param s: string to print
+    :return: None
+    """
     print(s, file=sys.stderr)
 
 
-def __print_rec(ldbrec):
-    print("%6d\t%4x\t%5d\t%04d\t%08x" %
-          (ldbrec.h, ldbrec.s, ldbrec.t, ldbrec.f, ldbrec.d))
+def hex2bytes(s: str) -> bytes:
+    """
+    Converts hex-string to bytes object
+    :param s: string to convert
+    :return: bytes converted to
+    """
+    return bytes.fromhex(s)
 
 
 def bytes2hex(b: bytes) -> str:
+    """
+    Convert bytes to hex-string
+    :param b: bytes to convert
+    :return: hex-string converted to
+    """
     return b.hex()
 
 
-def _get_varint(rec: bytes, pos: int = 0) -> tuple:
+def __get_varint(rec: bytes, pos: int = 0) -> tuple:
     """
-	Decode serialized varint.
-	Powered by Mark Friedenbach's [VarInt](https://github.com/maaku/python-bitcoin/blob/master/bitcoin/serialize.py)
-	@param rec - record itself
-	@param pos: position to start from
-	@return (value, bytes read)
-	"""
+    Decode serialized varint.
+    Powered by Mark Friedenbach's [VarInt](https://github.com/maaku/python-bitcoin/blob/master/bitcoin/serialize.py)
+    :param rec: record itself
+    :param pos: position to start from
+    :return (value, bytes read)
+    """
     result = 0
     l = len(rec)
     while pos <= l:
@@ -59,121 +79,184 @@ def _get_varint(rec: bytes, pos: int = 0) -> tuple:
             result += 1
         else:
             break
-    return (result, pos)
+    return result, pos
 
 
-def decode_rec(rec: bytes, bk_qty: int) -> object:
+def decode_rec(rec: bytes, bk_no: int) -> object:
     """
     Decode record.
     (see bitoin-core, chain.h, class CDiskBlockIndex)
+    :param rec: record to decode
+    :param bk_no: block no
+    :return: LdbRec object
     """
-    LdbRec.v, i = _get_varint(rec)  # 1. version
-    LdbRec.h, i = _get_varint(rec, i)  # 2. height
-    if (LdbRec.h >= bk_qty):
-        return None
-    LdbRec.s, i = _get_varint(rec, i)  # 3. status
-    LdbRec.t, i = _get_varint(rec, i)  # 4. nTx
+    LdbRec.v, i = __get_varint(rec)  # version
+    LdbRec.h, i = __get_varint(rec, i)  # height
+    if LdbRec.h != bk_no:
+        eprint("Bk # %d: wrong height (%d)" % (bk_no, LdbRec.h))
+    LdbRec.s, i = __get_varint(rec, i)  # status
+    LdbRec.t, i = __get_varint(rec, i)  # nTx
     LdbRec.f = LdbRec.d = LdbRec.u = None
-    if (LdbRec.s & (BLOCK_HAVE_DATA | BLOCK_HAVE_UNDO)):
-        LdbRec.f, i = _get_varint(rec, i)  # 5. File no
-        if (LdbRec.f > MAXFILENO):
-            eprint("Bk # %d: file no is too big: %d" % (LdbRec.h, LdbRec.f))
-    if (LdbRec.s & BLOCK_HAVE_DATA):
-        LdbRec.d, i = _get_varint(rec, i)  # 6. blk*.dat offset
-        if (LdbRec.d > TOOBIGOFFSET):
+    if LdbRec.s & (BLOCK_HAVE_DATA | BLOCK_HAVE_UNDO):
+        LdbRec.f, i = __get_varint(rec, i)  # File no
+        if LdbRec.f > MAXBKNO:
+            eprint("Bk # %d: file no is too big: %d" % (bk_no, LdbRec.f))
+    if LdbRec.s & BLOCK_HAVE_DATA:
+        LdbRec.d, i = __get_varint(rec, i)  # blk*.dat offset
+        if LdbRec.d > TOOBIGOFFSET:
             eprint("Bk # %d: data offset is too big: %d" % (bk_no, LdbRec.d))
-        elif (LdbRec.d < 8):
+        elif LdbRec.d < 8:
             eprint("Bk # %d: data offset cannot be < 8: %d" % (bk_no, LdbRec.d))
-    if (LdbRec.s & BLOCK_HAVE_UNDO):
-        LdbRec.u, i = _get_varint(rec, i)  # 7. rev*.dat offset
-        if (LdbRec.u > TOOBIGOFFSET):
+    if LdbRec.s & BLOCK_HAVE_UNDO:
+        LdbRec.u, i = __get_varint(rec, i)  # rev*.dat offset
+        if LdbRec.u > TOOBIGOFFSET:
             eprint("Bk # %d: undo offset is too big: %d" % (bk_no, LdbRec.u))
-        elif (LdbRec.u < 8):
+        elif LdbRec.u < 8:
             eprint("Bk # %d: undo offset cannot be < 8: %d" % (bk_no, LdbRec.u))
-    if (i != len(rec)):
+    if i != len(rec):
         eprint("Bk # %d: bytes decoded (%d) != record len (%d)." % (bk_no, i, len(rec)))
     return LdbRec
 
 
-def walk(ldn: str, ofn: str, qty: int, verbose: bool):
+def get_ldbrec(hash_b: bytes) -> bytes:
     """
-    @param ldn: LevelDB directory
-    @param ofn: output file name (2x4 bytes per block)
-    @param qty: Blocks process up to
+    Get LevelDB's record of bk. Excluding bk head (last 80 bytes)
+    :param hash_b: block record to find
+    :return: raw block record
+    """
+    global db
+    k = b'\x62' + hash_b
+    retvalue = None
+    ''' leveldb
+    try:
+        v = db.Get(k)  # bytearray; TODO: try...except(EOFError)
+    except KeyError:
+        eprint("Hash {} not found.".format(bytes2hex(hash_b)))
+    else:
+        retvalue = bytes(v[:-80])
+    '''
+    v = db.get(k)
+    if not v:
+        eprint("Hash {} not found.".format(bytes2hex(hash_b)))
+    else:
+        retvalue = v[:-80]
+    return retvalue
+
+
+def dump_rec(height: int, rec: bytes):
+    """
+    Save block record to file (to debug).
+    :param height: block no
+    :param rec: raw block record
+    :return:
+    """
+    with open("ldb/%06d.ldb" % height, "wb") as f:
+        f.write(rec)
+        f.close()
+
+
+def print_rec(ldbrec):
+    """
+    Print block record (to debug).
+    :param ldbrec: block record to print
+    :return: None
+    """
+    print("%d\t%d\t%x\t%d" %
+          (ldbrec.v, ldbrec.h, ldbrec.s, ldbrec.t), end='\t')
+    if ldbrec.f is None:
+        print("-----", end='\t')
+    else:
+        print("%05d" % ldbrec.f, end='\t')
+    if ldbrec.d:
+        print(ldbrec.d, end='\t')
+    else:
+        print("-----", end='\t')
+    if ldbrec.u:
+        print(ldbrec.u)
+    else:
+        print("-----")
+
+
+def walk(sfn: str, dfn: str, ldb: str, verbose: bool) -> int:
+    """
+    @param sfn: input file name (hashes hex strings (64 per line))
+    @param dfn: output file name (2x4 bytes per block)
+    @param ldb: LevelDB directory
     @param verbose: no comments
     @return: None
     """
-    # global LdbRec
+    global db
     # 0. prepare
+    sf = open(sfn, "rt")
+    if not sf:
+        eprint("Can't open %s to read" % sfn)
+        return 1
+    df = open(dfn, "wb")
+    if not df:
+        eprint("Can't open %s to write" % dfn)
+        return 1
+    # db = leveldb.LevelDB(ldb, create_if_missing=False)
+    db = plyvel.DB(ldb, create_if_missing=False)
     if verbose:  # header
-        print(header)
-        print(strike)
-    # 1. load
-    bk_count = bk_found = 0
-    bk_dict = dict()
-    db = plyvel.DB(ldn, create_if_missing=False)
-    for rec in db.iterator(include_key=False, prefix=b'\x62'):  # ^'b'
-        bk_count += 1
-        rec = decode_rec(rec[:-80], qty)
-        if not rec:
-            continue
-        r = bk_dict.get(rec.h, None)
-        if (r):
-            if (r[0] == rec.f and r[1] == rec.d):
-                continue
-            else:
-                eprint("Bk {} was already found. File/offset: was {}.{}, new {}.{}".format(rec.h, r[0], r[1], rec.f, rec.d))
-                break;
-        bk_dict[rec.h] = (rec.f, rec.d)
-        bk_found += 1
-        if verbose:
-            __print_rec(rec)
-        if bk_found >= qty:
+        print("Ver\tHeight\tStatus\tnTx\tFile\tDoffset\tUoffset")
+        print("===\t======\t======\t===\t====\t=======\t=======")
+    bk_no = 0
+    for line in sf:
+        # hash_b = sf.read(32)[::-1]  # 1. get bk hash
+        hash_b = hex2bytes(line.rstrip('\n'))
+        if not hash_b:
             break
-    db.close()
-    if (verbose):
-        print(strike)
-    if bk_found != qty:
-        eprint("Blocks found {} from required {}".format(bk_found, qty))
-        return
-    eprint("{}/{} blocks loaded.".format(bk_found, bk_count))
-    # 2. save
-    with open(ofn, "wb") as of:
-        for i in range(qty):
-            f, o = bk_dict[i]
-            of.write(pack("<I", f))
-            of.write(pack("<I", o))
+        rec = get_ldbrec(hash_b)  # 2. get ldb rec; FIXME: error handle
+        if not rec:
+            break
+        r = decode_rec(rec, bk_no)  # 3. decode it
+        df.write(pack("<I", r.f))  # 4. write_db
+        df.write(pack("<I", r.d))
+        if TODUMP:
+            dump_rec(bk_no, rec)
+        if verbose:
+            print_rec(r)
+        bk_no += 1
+    sf.close()
+    df.close()
+    if verbose:  # footer
+        print("===\t======\t======\t===\t====\t=======\t=======")
+    eprint("%s records ok." % bk_no)
+    return 0
 
 
 def init_cli():
-    """ Handle CLI """
-    parser = argparse.ArgumentParser(description='File-offset extractor.')
-    parser.add_argument('-l', '--ldb', metavar='<dir>', type=str, nargs=1,
+    """
+    Handle CLI
+    """
+    parser = argparse.ArgumentParser(description='Hash to file-offset.')
+    parser.add_argument('ldb', metavar='<dir>', type=str, nargs=1,
                         help='LevelDB dir')
-    parser.add_argument('-o', '--outfile', metavar='<file>', type=str, nargs=1,
-                        help='Output file name')
-    parser.add_argument('-n', '--num', metavar='n', type=int, nargs='?', default=1,
-                        help='Blocks to process (default=1)')
+    parser.add_argument('outfile', metavar='<file>', type=str, nargs=1,
+                        help='Bk locs file')
+    parser.add_argument('-i', '--infile', metavar='<file>', type=str, nargs=1, default=None,
+                        help='Input file')
     parser.add_argument('-v', '--verbose', action='store_true',
-                        help='Debug (default=false)')
+                        help='Debug process (default=false)')
     return parser
 
 
 def main():
-    """
-    CLI commands/options handler.
-    """
     parser = init_cli()
     args = parser.parse_args()
-    if (not args.ldb) or (not args.outfile):
+    # print(args.infile, type(args.infile))
+    if (not args.infile) or (not args.outfile) or (not args.ldb):
         parser.print_help()
-        return
-    ldir = args.ldb[0]
-    if not os.path.isdir(ldir):
-        eprint("LevelDB '{}' not exist or is not dir.".format(ldir))
-        return
-    ofile = args.outfile[0]
-    return walk(ldir, ofile, args.num, args.verbose)
+    else:
+        ifile = args.infile[0]
+        ofile = args.outfile[0]
+        ldir = args.ldb[0]
+        if not os.path.isfile(ifile):
+            eprint("Infile '{}' not exist or is not file.".format(ifile))
+        elif not os.path.isdir(ldir):
+            eprint("LevelDB '{}' not exist or is not dir.".format(ldir))
+        else:
+            return walk(ifile, ofile, ldir, args.verbose)
 
 
 if __name__ == '__main__':
