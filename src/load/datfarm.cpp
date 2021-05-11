9bfd4ef566d408100ddadff01fd2f7bc56fd539f
Eugene <ti.eugene@gmail.com>
2021-05-11 14:31:55 +0300

    Blocks loader code refactoring

--- src/load/datfarm.cpp
+++ src/load/datfarm.cpp
@@ -6,45 +6,38 @@
 #include <queue>
 
 #include "datfarm.h"
+#include "common.h"
 
 using namespace std;
 
 const uint8_t WIN_SIZE = 16;
 static queue<size_t> opened;
 
-bool        DATFARM_T::open(const size_t no)
-{
-    if (no > qty) {
-        cerr << "File # too big: " << no << endl;
-        return false;
-    }
+bool  DATFARM_T::open(const size_t no) {
+    if (no > qty)
+      return b_error("File # too big: " + to_string(no));
     if (file[no].is_open())
-        return true;
+      return true;
     if (opened.size() >= WIN_SIZE) {  // close unused file
-        file[opened.front()].close();
-        opened.pop();
+      file[opened.front()].close();
+      opened.pop();
     }
     ostringstream ss;
     ss << setw(5) << setfill('0') << no;
     string fn = folder + "blk" + ss.str() + ".dat";
     file[no].open(fn, ios::in|ios::binary);
-    if (!file[no].is_open()) {
-        cerr << "Can't open file " << fn << endl;
-        return false;
-    }
+    if (!file[no].is_open())
+      return b_error("Can't open file " + fn);
     opened.push(no);
     return true;
 }
 
-bool        DATFARM_T::read(const size_t no, const size_t offset, const int size, void *dst)
-{
+bool  DATFARM_T::read(const size_t no, const size_t offset, const int size, void *dst) {
     if (!open(no))
-        return false;
+      return false;
     file[no].seekg(offset, file[no].beg);
     file[no].read(static_cast<char *>(dst), size);
-    if (file[no].gcount() != size) {
-        cerr << "Can't read " << size << " bytes from " << no << "." << offset << endl;
-        return false;
-    }
+    if (file[no].gcount() != size)
+      return b_error("Can't read " + to_string(size) + " bytes from " + to_string(no) + "." + to_string(offset));
     return true;
 }
