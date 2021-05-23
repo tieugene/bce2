b3c7696d3e862b187fb94e7607e5bba6f2774faf
Eugene <ti.eugene@gmail.com>
2021-05-23 17:53:29 +0300

    P2PKH decoder implemneted

--- src/bk/addr.cpp
+++ src/bk/addr.cpp
@@ -11,24 +11,21 @@
 
 using namespace std;
 
-/// dummy
-void dump_script(const string s) { cerr << s << endl; }
-
 /// check PK (uncompressed) prefix
 inline bool check_PKu_pfx(const u8_t pfx) {
-    /*
-     * Prefix byte must be 0x04, but there are some exceptions, e.g.
-     * bk 230217
-     * tx 657aecafe66d729d2e2f6f325fcc4acb8501d8f02512d1f5042a36dd1bbd21d1
-     * vouts 34 (7), 100 (6), 158 (7), 360 (6)
-     * but *not* 5 (vouts 189, 242)
-     */
-    return (pfx & 0xFC) == 0x04 and pfx != 0x05;
+  /*
+   * Prefix byte must be 0x04, but there are some exceptions, e.g.
+   * bk 230217
+   * tx 657aecafe66d729d2e2f6f325fcc4acb8501d8f02512d1f5042a36dd1bbd21d1
+   * vouts 34 (7), 100 (6), 158 (7), 360 (6)
+   * but *not* 5 (vouts 189, 242)
+   */
+  return (pfx & 0xFC) == 0x04 and pfx != 0x05;
 }
 
 /// check PK (compressed) prefix (must be 2..3)
 inline bool check_PKc_pfx(const u8_t pfx) {
-    return (pfx & 0xFE) == 0x02;
+  return (pfx & 0xFE) == 0x02;
 }
 
 ADDR_PK_T::ADDR_PK_T(string_view script) {
@@ -51,25 +48,31 @@ ADDR_PK_T::ADDR_PK_T(string_view script) {
 }
 
 ADDR_PKH_T::ADDR_PKH_T(string_view script) {
-  throw AddrException("Bad P2PKH not implemented");
+  if (script.length() == 25 and       // was >= dirty hack for 71036.?.? and w/ OP_NOP @ end
+      u8_t(script[1]) == OP_HASH160 and
+      u8_t(script[2]) == 20 and
+      u8_t(script[23]) == OP_EQUALVERIFY and
+      u8_t(script[24]) == OP_CHECKSIG)
+    memcpy(&data, script.data() + 3, sizeof (uint160_t));
+  else
+    throw AddrException("Bad P2PKH");
 }
 
 ADDR_SH_T::ADDR_SH_T(string_view script) {
-  throw AddrException("Bad P2SH not implemented");
+  throw AddrException("P2SH not implemented");
 }
 
 ADDR_WPKH_T::ADDR_WPKH_T(string_view script) {
-  throw AddrException("Bad P2WPKH not implemented");
+  throw AddrException("P2WPKH not implemented");
 }
 
 ADDR_WSH_T::ADDR_WSH_T(string_view script) {
-  throw AddrException("Bad P2WSH not implemented");
+  throw AddrException("P2WSH not implemented");
 }
 
 ADDR_MS_T::ADDR_MS_T(string_view script) {
-  throw AddrException("Bad P2MS not implemented");
+  throw AddrException("P2MS not implemented");
 }
-
 
 ADDR_BASE_T *addr_decode(string_view data) {  // sript, size
   // FIXME: empty script
@@ -80,38 +83,38 @@ ADDR_BASE_T *addr_decode(string_view data) {  // sript, size
   if (data.length() < 22)             // P2WPKH is smallest script
     return nullptr;
   switch (opcode) {
-  case 0x41:
+    case 0x41:
       retvalue = new ADDR_PK_T(data); // uncompressed
       break;
-  case 0x21:
+    case 0x21:
       retvalue = new ADDR_PK_T(data); // compressed
       break;
-  case OP_DUP:
+    case OP_DUP:
       retvalue = new ADDR_PKH_T(data);
       break;
-  case OP_HASH160:
+    case OP_HASH160:
       retvalue = new ADDR_SH_T(data);
       break;
-  case OP_0:                          // P2W* ver.0 (BIP-141)
+    case OP_0:                          // P2W* ver.0 (BIP-141)
       switch (data[1]) {
         case 0x14:
-            retvalue = new ADDR_WPKH_T(data); // uint160_t
-            break;
+          retvalue = new ADDR_WPKH_T(data); // uint160_t
+          break;
         case 0x20:
-            retvalue = new ADDR_WSH_T(data);  // uint256_t
-            break;
+          retvalue = new ADDR_WSH_T(data);  // uint256_t
+          break;
         default:
-            dump_script("Bad P2Wx");
+          throw AddrException("Bad P2Wx");
       }
       break;
-  case OP_1 ... OP_16:
+    case OP_1 ... OP_16:
       retvalue = new ADDR_MS_T(data);
       break;
-  default:
+    default:
       if (opcode <= 0xB9) // x. last defined opcode
-          dump_script("Not impl-d");
+        throw AddrException("Not impl-d");
       else
-          dump_script("Invalid");
+        throw AddrException("Invalid");
   }
   return retvalue;
 }
